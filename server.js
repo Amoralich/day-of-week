001b4dcaf00a9b57072adb56eeaf095cae2a5684
Opilane TTHK <opilane@tthk.ee>
2020-02-11 10:04:06 +0200

    ejs fail

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello Word!");
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs")
+
+function get_day_of_week(date){
+   var get_day_of_week = ["sunday", "Mondey","Thusdey","Wendsdey"];
+
+    return get_day_of_week[date.getDay()];
+    }
+
+    app.get("/today", function(req,res){
+    var today = new Date;
+    //res.send(get_day_of_week(today));
+    res.render('day_of_week',{date: today, day_of_week: get_day_of_week(today)});
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
