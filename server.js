5b9c1fafb33e0a35b8d098010668db9e6927cea6
Opilane TTHK <opilane@tthk.ee>
2020-02-11 10:39:12 +0200

    week

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 app.set("view engine", "ejs")
 
 function get_day_of_week(date){
-   var get_day_of_week = ["sunday", "Mondey","Thusdey","Wendsdey"];
+   var get_day_of_week = ["sunday", "Mondey","Thusdey","Wendsdey","Thursday","Friday","Saturday"];
 
     return get_day_of_week[date.getDay()];
     }
 
-    app.get("/today", function(req,res){
-    var today = new Date;
+    app.get("/birthday", function(req,res){
+    var birthday = new Date(2002,6,16);
     //res.send(get_day_of_week(today));
-    res.render('day_of_week',{date: today, day_of_week: get_day_of_week(today)});
+    res.render('day_of_week',{
+        date: birthday, 
+        day_of_week: get_day_of_week(birthday)});
 });
 
 app.listen(3000, function(){
